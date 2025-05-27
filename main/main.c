99f484af1053e6e67af08f684ec8a89c61ac0d9e
mark <marklendering@gmail.com>
2025-05-28 00:25:11 +0200

    Reverted back to default wallpaper. Updated GUI layout and added buttons that can be navigated. Display title and time on wallpaper when in inactive mode. cleaned up code.

--- main/main.c
+++ main/main.c
@@ -6,6 +6,7 @@
 #include "bsp/input.h"
 #include "bsp/led.h"
 #include "bsp/power.h"
+#include "bsp/rtc.h"
 #include "bsp/tanmatsu.h"
 #include "driver/gpio.h"
 #include "driver/uart.h"
@@ -56,12 +57,14 @@ static pax_buf_t                    fb                   = {0};
 static esp_lcd_panel_handle_t    lcd_panel         = NULL;
 static QueueHandle_t                input_event_queue    = NULL;
 SemaphoreHandle_t line_mutex;
+esp_mqtt_client_handle_t client = NULL;
 
-extern uint8_t const wallpaper_start[] asm("_binary_wallpaper_event_notifier_png_start");
-extern uint8_t const wallpaper_end[] asm("_binary_wallpaper_event_notifier_png_end");
+extern uint8_t const wallpaper_start[] asm("_binary_wallpaper_png_start");
+extern uint8_t const wallpaper_end[] asm("_binary_wallpaper_png_end");
 
+bool inactive_show_time = false;
 
-#define num_lines 17
+#define num_lines 5
 #define num_chars 60
 
 char line_buffers[num_lines][num_chars];
@@ -69,12 +72,30 @@ int  line_index[num_lines];
 char input_buffer[num_chars];
 uint8_t led_buffer[6 * 3] = {0};
 
+time_t now_time;
+struct tm timeinfo;
+
 uint8_t msg_led_cnt = 0;
 bool mqtt_msg_event = false;
 bool mqtt_msg_transmit = false;
 bool wifi_connected = false;
 bool wifi_connecting = false;
 bool sd_card_present = false;
+
+
+#define HEADER_HEIGHT  30
+#define FOOTER_HEIGHT  30
+#define BUTTON_WIDTH   100
+#define BUTTON_HEIGHT  100
+#define BUTTON_GAP     20
+#define TEXT_FIELD_HEIGTH  120
+
+
+const char* menu_title = "Event Notifier";
+const char* connection_status = "Wi-Fi: Connected";
+const char* footer_text = "Use left/right to navigate. Press return to select.";
+const char* buttons[] = {"Button1", "Button2", "Button3"};
+#define NUM_BUTTONS 3
 
 
 void init(void) {
@@ -99,18 +120,6 @@ void add_line(char* text) {
     }
 }
 
-// static void wifi_task(void* pvParameters) {
-//     if (wifi_remote_initialize() == ESP_OK) {
-//         wifi_connection_init_stack();
-//         wifi_connect_try_all();
-//     } else {
-//         bsp_power_set_radio_state(BSP_POWER_RADIO_STATE_OFF);
-//         ESP_LOGE(TAG, "WiFi radio not responding, did you flash ESP-HOSTED firmware?");
-//     }
-//     wifi_stack_task_done = true;
-//     vTaskDelete(NULL);
-// }
-
 void set_led_color(uint8_t led, uint32_t color) {
     led_buffer[led * 3 + 0] = (color >> 8) & 0xFF;  // G
     led_buffer[led * 3 + 1] = (color >> 16) & 0xFF; // R
@@ -119,13 +128,11 @@ void set_led_color(uint8_t led, uint32_t color) {
 
 static void led_task(void* pvParameters) {
     while (1) {
-        set_led_color(0, LED_GREEN); // power led, change color depending on battery level
         if(wifi_connected) set_led_color(1, LED_GREEN); // wifi led
         else if(wifi_connecting) set_led_color(1, LED_YELLOW);
         else set_led_color(1, LED_RED);
 
         if(mqtt_msg_event) {
-            //todo: toggle led with 1hz frequency
             if(msg_led_cnt % 2 == 0) {
                 set_led_color(2, LED_YELLOW); // message led
             } else {
@@ -156,49 +163,138 @@ static void led_task(void* pvParameters) {
     }
 }
 
-void blit() {
-    // pax_background(&fb, 0xFF64E38F);
-    pax_background(&fb, 0xFFFFFFFF);
-    printf("----\r\n");
+int selected_button = 0;
+
+// Example callback handlers
+void button1_action() {
+    printf("Button 1 pressed!\n");
+    if(client) esp_mqtt_client_publish(client, "/esp32/coffee", "button 1", 0, 1, 0);
+}
+
+void button2_action() {
+    printf("Button 2 pressed!\n");
+    if(client) esp_mqtt_client_publish(client, "/esp32/coffee", "button 2", 0, 1, 0);
+}
+
+void button3_action() {
+    printf("Button 3 pressed!\n");
+    if(client) esp_mqtt_client_publish(client, "/esp32/coffee", "button 3", 0, 1, 0);
+}
+
+// Hook button callbacks
+void (*button_callbacks[])() = {
+    button1_action,
+    button2_action,
+    button3_action
+};
+
+// Drawing functions
+void draw_header(pax_buf_t *buf) {
+    // pax_draw_rect(buf, pax_col_rgb(50, 50, 200), 0, 0, display_v_res, HEADER_HEIGHT);
+
+    //	pax_buf_t *buf, pax_col_t color, float x0, y0, x1, y1
+    pax_draw_line(buf, 0xFF2B2C3A, 10, HEADER_HEIGHT, display_v_res - 20, HEADER_HEIGHT);
+    pax_draw_text(buf, 0xFF2B2C3A, pax_font_sky_mono, 18, 5, 5, menu_title);
+    pax_draw_text(buf, 0xFF2B2C3A, pax_font_sky_mono, 18, display_h_res - 35, 5, connection_status);
+}
+
+void draw_footer(pax_buf_t *buf) {
+    pax_draw_line(buf, 0xFF2B2C3A, 10, display_h_res - FOOTER_HEIGHT, display_v_res - 20, display_h_res - FOOTER_HEIGHT);
+
+    // pax_draw_rect(buf, pax_col_rgb(30, 30, 200), 0, display_h_res - FOOTER_HEIGHT, display_v_res, FOOTER_HEIGHT);
+    pax_draw_text(buf, 0xFFFFFFFF, pax_font_sky_mono, 16, 5, display_v_res - FOOTER_HEIGHT, footer_text);
+}
+
+void draw_buttons(pax_buf_t *buf) {
+    float start_x = (display_h_res - (NUM_BUTTONS * BUTTON_WIDTH + (NUM_BUTTONS - 1) * BUTTON_GAP)) / 2;
+    float y = HEADER_HEIGHT + 40;
+
+    for (int i = 0; i < NUM_BUTTONS; i++) {
+        // pax_col_t color = (i == selected_button) ? pax_col_rgb(255, 255, 0) : pax_col_rgb(100, 100, 100);
+        pax_col_t color = pax_col_rgb(100, 100, 100);
+        pax_col_t highlight_color = pax_col_rgb(150, 150, 150);
+        float x = start_x + i * (BUTTON_WIDTH + BUTTON_GAP);
+        pax_outline_rect(buf, color, x, y, BUTTON_WIDTH, BUTTON_HEIGHT);
+        if((i == selected_button))
+            pax_draw_rect(buf, highlight_color, x, y, BUTTON_WIDTH, BUTTON_HEIGHT);
+        pax_draw_text(buf, 0xFF000000, pax_font_sky_mono, 16, x + 10, y + 12, buttons[i]);
+    }
+}
+
+void draw_text_field(pax_buf_t *buf){
+    uint16_t offset_y = display_h_res - FOOTER_HEIGHT - TEXT_FIELD_HEIGTH;
+    // pax_draw_rect(buf, 0xFF2B2C3A, 0, offset_y, display_v_res, TEXT_FIELD_HEIGTH);
     for (int line = 0; line < num_lines; line++) {
         if (line_index[line] >= 0) {
             printf("%d: %d = %s\r\n", line, line_index[line], line_buffers[line_index[line]]);
-            pax_draw_text(&fb, 0xFF2B2C3A, pax_font_sky_mono, 24, 0, 24 * line, line_buffers[line_index[line]]);
+            pax_draw_text(buf, 0xFF2B2C3A, pax_font_sky_mono, 18, 5, offset_y + (24 * line), line_buffers[line_index[line]]);
         } else {
             printf("%d: (empty)\r\n", line);
         }
     }
-    printf("\r\n");
-    pax_draw_rect(&fb, 0xFFFFFFFF, 0, pax_buf_get_height(&fb) - 24, pax_buf_get_width(&fb), 24);
-    pax_draw_text(&fb, 0xFF000000, pax_font_sky_mono, 24, 0, pax_buf_get_height(&fb) - 24, input_buffer);
+}
+
+void selectNextButton(bool indexRight)
+{
+    if(indexRight)
+    {
+        selected_button++;
+
+    } else {
+        selected_button--;
+    }
+
+    if(selected_button > 2) selected_button = 0;
+    else if (selected_button < 0) selected_button = 2;
+}
+
+void render_gui() {
+    pax_background(&fb, pax_col_rgb(220, 220, 220));
+    draw_header(&fb);
+    draw_buttons(&fb);
+    draw_footer(&fb);
+    draw_text_field(&fb);
     esp_lcd_panel_draw_bitmap(lcd_panel, 0, 0, display_h_res, display_v_res, pax_buf_get_pixels(&fb));
+}
+
+void render_wallpaper_clock(bool includeClock) {
+    pax_insert_png_buf(&fb, wallpaper_start, wallpaper_end - wallpaper_start, 0, 0, 0);
+    pax_draw_text(&fb, 0xFFFFFFFF, pax_font_sky_mono, 40, 180, 380, menu_title);
+    //TODO: render text Event Notifier on wallpper
+    if(includeClock){
+        char strftime_buf[64];
+
+        time(&now_time);
+        localtime_r(&now_time, &timeinfo);
+        strftime(strftime_buf, sizeof(strftime_buf), "%H:%M:%S", &timeinfo);
+        pax_draw_text(&fb, 0xFFFFFFFF, pax_font_sky_mono, 100, 100, 140, strftime_buf);
+    }
+    //TODO: show current time 
+    esp_lcd_panel_draw_bitmap(lcd_panel, 0, 0, display_h_res, display_v_res, pax_buf_get_pixels(&fb));
+
+}
+
+void blit() {
+    if(!inactive_show_time) render_gui();
+    else render_wallpaper_clock(true);
 }
 
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
     esp_mqtt_event_handle_t event = event_data;
-    esp_mqtt_client_handle_t client = event->client;
 
     switch ((esp_mqtt_event_id_t)event_id) {
         case MQTT_EVENT_CONNECTED:
+            client = event->client;
             ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
             esp_mqtt_client_subscribe(client, "/esp32/coffee", 0);
-            esp_mqtt_client_publish(client, "/esp32/coffee", "Hello MQTT", 0, 1, 0);
             mqtt_msg_transmit = true;
-            // add_line("published data");
-            // blit();
             break;
         case MQTT_EVENT_DATA:
             char buffer[num_chars];
             memset(buffer, 0, sizeof(buffer));
-            // snprintf(buffer, sizeof(buffer), "topic: %.*s\r\n", event->topic_len, event->topic);
-            // add_line(buffer);
-            // ESP_LOGI(TAG, "MQTT_EVENT_DATA");
             snprintf(buffer, sizeof(buffer), "data: %.*s\r\n", event->data_len, event->data);
             add_line(buffer);
-            blit();
-            // printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-            // printf("DATA=%.*s\r\n", event->data_len, event->data);
             mqtt_msg_event = true;
             break;
         default:
@@ -216,11 +312,6 @@ static void mqtt_app_start(void) {
     esp_mqtt_client_start(client);
 }
 
-void wallpaper(int h_res, int v_res) {
-    pax_insert_png_buf(&fb, wallpaper_start, wallpaper_end - wallpaper_start, 0, 0, 0);
-    esp_lcd_panel_draw_bitmap(lcd_panel, 0, 0, h_res, v_res, pax_buf_get_pixels(&fb));
-}
-
 void app_main(void) {
     init();
     // Start the GPIO interrupt service
@@ -236,6 +327,7 @@ void app_main(void) {
 
     // Initialize the Board Support Package
     ESP_ERROR_CHECK(bsp_device_initialize());
+
 
     ESP_LOGW(TAG, "Switching radio off...\r\n");
     bsp_power_set_radio_state(BSP_POWER_RADIO_STATE_OFF);
@@ -287,7 +379,9 @@ void app_main(void) {
     pax_buf_reversed(&fb, display_data_endian == LCD_RGB_DATA_ENDIAN_BIG);
     pax_buf_set_orientation(&fb, orientation);
 
-    wallpaper(display_h_res, display_v_res);
+    render_wallpaper_clock(false);
+
+    
     
     // Get input event queue from BSP
     ESP_ERROR_CHECK(bsp_input_get_queue(&input_event_queue));
@@ -320,7 +414,6 @@ void app_main(void) {
             wifi_connected = true;
             esp_netif_ip_info_t* ip_info = wifi_get_ip_info();
             add_line(ip4addr_ntoa((const ip4_addr_t*)&ip_info->ip));
-            blit();
         } 
     } else {
         bsp_power_set_radio_state(BSP_POWER_RADIO_STATE_OFF);
@@ -334,8 +427,8 @@ void app_main(void) {
 
     while (1) {
         //TODO: 
-        // 1. Show big clock by default, when any button is pressed show graphical interface
-        // 2. Add graphics interface with buttons.
+        // 1. Show big clock by default, when any button is pressed show graphical interface - done
+        // 2. Add graphics interface with buttons. - done
         // 3. Parse json data received from mqtt
         // 4. generate json data to be transmitted
         // 5. Add encryption for messages
@@ -344,55 +437,56 @@ void app_main(void) {
         // 8. Add wallpaper - done
 
         bsp_input_event_t event;
-        if (xQueueReceive(input_event_queue, &event, portMAX_DELAY) == pdTRUE) {
+        if (xQueueReceive(input_event_queue, &event, pdMS_TO_TICKS(50)) == pdTRUE) {
             switch (event.type) {
-                case INPUT_EVENT_TYPE_KEYBOARD: {
-                    char ascii = event.args_keyboard.ascii;
-                    if (ascii == '\b') {
-                        size_t length = strlen(input_buffer);
-                        if (length > 0) {
-                            input_buffer[length - 1] = '\0';
-                        }
-                    } else if (ascii == '\r' || ascii == '\n') {
-                        // Ignore
-                    } else {
-                        size_t length = strlen(input_buffer);
-                        if (length < num_chars - 1) {
-                            input_buffer[length]     = ascii;
-                            input_buffer[length + 1] = '\0';
-                        }
-                    }
-                    // blit();
-                    break;
-                }
+                // case INPUT_EVENT_TYPE_KEYBOARD: {
+                //     char ascii = event.args_keyboard.ascii;
+                //     if (ascii == '\b') {
+                //         size_t length = strlen(input_buffer);
+                //         if (length > 0) {
+                //             input_buffer[length - 1] = '\0';
+                //         }
+                //     } else if (ascii == '\r' || ascii == '\n') {
+                //         // Ignore
+                //     } else {
+                //         size_t length = strlen(input_buffer);
+                //         if (length < num_chars - 1) {
+                //             input_buffer[length]     = ascii;
+                //             input_buffer[length + 1] = '\0';
+                //         }
+                //     }
+                //     break;
+                // }
                 case INPUT_EVENT_TYPE_NAVIGATION: {
                     if (event.args_navigation.state) {
-                        /*if (event.args_navigation.key == BSP_INPUT_NAVIGATION_KEY_BACKSPACE) {
-                            size_t length = strlen(input_buffer);
-                            if (length > 0) {
-                                input_buffer[length - 1] = '\0';
-                            }
-                        }*/
-                        if (event.args_navigation.key == BSP_INPUT_NAVIGATION_KEY_RETURN) {
-                            // uart_write_bytes(RADIO_UART, input_buffer, strlen(input_buffer));
-                            // uart_write_bytes(RADIO_UART, "\r\n", 2);
+                        switch (event.args_navigation.key) {
+                            case BSP_INPUT_NAVIGATION_KEY_F1:
+                                if(client) esp_mqtt_client_publish(client, "/esp32/coffee", "I require coffee!", 0, 1, 0);
+                                mqtt_msg_transmit = true;
+                                break;
+                            case BSP_INPUT_NAVIGATION_KEY_RIGHT:
+                                selectNextButton(true);
+                                break;
+                            case BSP_INPUT_NAVIGATION_KEY_LEFT:
+                                selectNextButton(false);
+                                break;
+                            case BSP_INPUT_NAVIGATION_KEY_RETURN:
+                                button_callbacks[selected_button]();
+                                break;
+                            case BSP_INPUT_NAVIGATION_KEY_ESC:
+                                inactive_show_time ^= 1;
+                                break;
+                            default:
+                            break;
 
-                            // uart_write_bytes(RADIO_UART, (const char*)mock_tcp_packet, sizeof(mock_tcp_packet));
-
-                            add_line(input_buffer);
-                            memset(input_buffer, 0, num_chars);
                         }
-                        // blit();
-                        // if (event.args_navigation.key == BSP_INPUT_NAVIGATION_KEY_F1) {
-                        //     wallpaper(h_res, v_res);
-                        // }
                     }
                     break;
                 }
                 default:
                     break;
             }
-        } 
+        }
         blit();
     }
 }
